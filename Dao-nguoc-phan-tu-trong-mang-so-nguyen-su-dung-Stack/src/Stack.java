cf40885251e7a8b56927d4dc0cf9fed011d26c0c
hung8695 <hung8695@gmail.com>
2021-05-20 15:55:33 +0700

    Bai tap quan ly sinh vien

--- Dao-nguoc-phan-tu-trong-mang-so-nguyen-su-dung-Stack/src/Stack.java
+++ Dao-nguoc-phan-tu-trong-mang-so-nguyen-su-dung-Stack/src/Stack.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Stack {
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+
+public class Stack<T> {
+    private LinkedList<T> stack;
+    public Stack(){
+        stack=new LinkedList<>();
+    }
+    public void push(T element){
+        stack.addFirst(element);
+        public T pop(){
+            if(isEmpty()){
+                throw new EmptyStackException();
+            }
+            return stack.removeFirst();
+        }
+    }
 }
